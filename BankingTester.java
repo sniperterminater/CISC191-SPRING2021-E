fd04314afa65cc62a275ba7c0c65ac47e4eeabba
sniperterminater <78382598+sniperterminater@users.noreply.github.com>
2021-05-18 12:46:03 -0700

    Update and rename tester.java to BankingTester.java

--- BankingTester.java
+++ BankingTester.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class tester {
+public class BankingTester {
     public static void main(String[] args){
         Banking user1 = new Banking();
         user1.start();
